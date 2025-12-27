107f579d826067f658babe49308e968483bc4eae
dopeFuneral <125204952+dopeFuneral@users.noreply.github.com>
2025-12-27 23:27:45 +0530

    third commit

---
+++ basics/prac1.js
@@ -0,0 +1,5 @@
+console.log("Hello, World!");
+console.log("This is my first JavaScript program.");
+console.log("I am learning how to code!");
+console.log("JavaScript is fun!");
+console.log("Goodbye, World!");
