6e99abea5389a3c7f2335a5a793f8cce81b88561
dopeFuneral <125204952+dopeFuneral@users.noreply.github.com>
2025-12-28 00:16:09 +0530

    datatypes

---
+++ basics/datatypes.js
@@ -0,0 +1,7 @@
+"use strict"; //treats all code as modern JS\
+
+console.log(typeof "rio"); //shows string
+console.log(typeof 34); //shows number
+console.log(typeof true); //shows boolean
+console.log(typeof undefined); //shows undefined    
+console.log(typeof null); //shows object (this is a bug in JS)
